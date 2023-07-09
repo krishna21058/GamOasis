60e7ed67a9d27fa86a27cc47d62e7e36b88b31ee
Krishna Somani <krishna21058@iiitd.ac.in>
2023-07-10 00:57:31 +0530

    Merging all games and some other fixes

--- Rock-Paper-Scissors/script.js
+++ Rock-Paper-Scissors/script.js
@@ -5,18 +5,21 @@ function final_result(pscore, cscore) {
   let result = document.getElementById('round-result')
   console.log("res")
   if (Number(pscore.innerText) == 10) {
-      let finres = document.getElementById("final-result")
-      finres.innerText = "You Win!!"
+    let finres = document.getElementById("final-result")
+    finres.innerText = "You Win! 🎉"
+    finres.style.color = "greenyellow"
 
-      result.innerText =""
-      disableButtons()
+    result.innerText = ""
+    disableButtons()
+    finres.classList.add("wave-animation");
   }
   if (Number(cscore.innerText) == 10) {
-      let finres = document.getElementById("final-result")
-      finres.innerText = "You Lose!!"
+    let finres = document.getElementById("final-result")
+    finres.innerText = "You Lose!!"
+    finres.style.color = "red"
 
-      result.innerText =""
-      disableButtons()
+    result.innerText = ""
+    disableButtons()
   }
 }
 
@@ -35,15 +38,22 @@ function disableButtons() {
 function showResult(score, playerChoice, computerChoice) {
   let result = document.getElementById('round-result')
   switch (score) {
-      case -1:
-          result.innerText = `Computer gets 1 point!`
-          break;
-      case 0:
-          result.innerText = `Draw!`
-          break;
-      case 1:
-          result.innerText = `You get 1 point!`
-          break;
+    case -1:
+      result.classList.remove("wave-animation");
+      result.style.color = "red"
+      result.innerText = `Computer gets 1 point!`
+      break;
+    case 0:
+      result.classList.remove("wave-animation");
+      result.style.color = "white"
+      result.innerText = `Draw!`
+      break;
+    case 1:
+      result.style.color = "greenyellow"
+      result.classList.remove("wave-animation");
+      result.classList.add("wave-animation");
+      result.innerText = `You get 1 point!`
+      break;
   }
 }
 
@@ -51,23 +61,23 @@ function showResult(score, playerChoice, computerChoice) {
 function getResult(playerChoice, computerChoice, pscore, cscore) {
   let score = 0
   if (playerChoice == computerChoice) {
-      score = 0
+    score = 0
   } else if (playerChoice == 'Rock' && computerChoice == 'Scissors') {
-      pscore.innerText = Number(pscore.innerText) + 1
-      score = 1
+    pscore.innerText = Number(pscore.innerText) + 1
+    score = 1
 
   } else if (playerChoice == "Paper" && computerChoice == "Rock") {
 
-      pscore.innerText = Number(pscore.innerText) + 1
-      score = 1
+    pscore.innerText = Number(pscore.innerText) + 1
+    score = 1
   } else if (playerChoice == "Scissors" && computerChoice == "Paper") {
 
-      pscore.innerText = Number(pscore.innerText) + 1
-      score = 1
+    pscore.innerText = Number(pscore.innerText) + 1
+    score = 1
   } else {
 
-      score = -1
-      cscore.innerText = Number(cscore.innerText) + 1
+    score = -1
+    cscore.innerText = Number(cscore.innerText) + 1
 
   }
   return score
@@ -91,7 +101,7 @@ function onClick(playerChoice, pscore, cscore) {
   showResult(score, playerChoice.value, computerChoice)
   if ((Number(pscore.innerText) == 10) || Number(cscore.innerText) == 10) {
     final_result(pscore, cscore)
-}
+  }
 
 }
 
@@ -105,14 +115,15 @@ function endGame() {
   let paper = document.getElementById("paper")
   let scissors = document.getElementById("scissors")
 
-  scissors.disabled=false
-  rock.disabled=false
-  paper.disabled=false
+  scissors.disabled = false
+  rock.disabled = false
+  paper.disabled = false
 
   playerScore.innerText = 0
   compScore.innerText = 0
   rresult.innerText = ''
   fresult.innerText = ''
+  result.classList.remove("wave-animation")
 }
 
 
@@ -134,7 +145,6 @@ function playGame() {
   let rButton = document.getElementById('restart')
   rButton.onclick = () => endGame()
   let quit = document.getElementById('quit')
-  quit.onclick = () => exit
 }
 
 playGame()
